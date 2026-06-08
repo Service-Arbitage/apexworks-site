5860f44f8673d4df43ef9eebdfe409b741dbb797
Yung T <testrun1269@gmail.com>
2026-06-08 16:53:44 -0700

    site update

---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "C:\Users\Abosede\OneDrive\Desktop\My Website"
+git add .
+git commit -m "site update"
+git push
+echo.
+echo Done! Your site will be live in about 60 seconds.
+pause
